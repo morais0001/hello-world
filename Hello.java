cb736248400cc95b8d5783c0fb0361a84e5072f3
el15morais <gabriel_morais57@hotmail.com>
2025-03-07 20:58:06 -0300

    Ola-mundo

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        olaMundo();
+    }
+
+    public static void olaMundo(){
+        System.out.println("Olá mundo");
     }
 }
