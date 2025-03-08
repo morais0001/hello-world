9c470f9cf3f9fc22c4cc01444edc73c00d9c5ff2
el15morais <gabriel_morais57@hotmail.com>
2025-03-07 21:01:50 -0300

    Hola-que-tal

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        holaQueTal();
+    }
+
+    public static void holaQueTal(){
+        System.out.println("Hola que tal");
     }
 }
